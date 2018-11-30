a8cb93d67a5313097b7305b0a228949e3a0c4108
Federico Zivolo <fzivolo@quid.com>
2018-11-30 12:32:47 +0100

    chore: more deploy fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,10 @@
+# Setup Git user for automatic deploy
+git config --global user.email "automated+write@quid.com"
+git config --global user.name "automated.write"
+
+# Override the read-only npmrc with one with write permissions
+# the auth key is stored in Travis CI
 bash scripts/npmrc.sh automated.write automated+write@quid.com $AUTOMATED_WRITE_AUTH > .npmrc
+
+# Bump versions, commit, push to git, publish to npm/jfrog
 lerna publish
--- scripts/npmrc.sh
+++ scripts/npmrc.sh
@@ -5,7 +5,7 @@ TOKEN=$1
 cat << EOF
 @quid:registry=https://quid.jfrog.io/quid/api/npm/npm-quid/
 //quid.jfrog.io/quid/api/npm/npm-quid/:_auth=$TOKEN
-//quid.jfrog.io/quid/api/npm/npm-quid/:username=automated.read
-//quid.jfrog.io/quid/api/npm/npm-quid/:email=automated+read@quid.com
+//quid.jfrog.io/quid/api/npm/npm-quid/:username=automated.write
+//quid.jfrog.io/quid/api/npm/npm-quid/:email=automated+write@quid.com
 //quid.jfrog.io/quid/api/npm/npm-quid/:always-auth=true
 EOF
