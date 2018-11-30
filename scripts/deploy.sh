66b8ab2bee450a39616156a08559723122c23d7f
Federico Zivolo <fzivolo@quid.com>
2018-11-30 14:56:42 +0100

    chore: checkout master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,5 +12,8 @@ yarn config set username $NAME
 yarn config set email $EMAIL
 yarn config set _auth $AUTH
 
+# Lerna can publish only if on master, Travis CI goes in detached state initially
+git checkout master
+
 # Bump versions, commit, push to git, publish to npm/jfrog
 $(yarn bin)/lerna publish --no-verify-access --registry https://quid.jfrog.io/quid/api/npm/npm-quid/
