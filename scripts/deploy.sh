60ebf1be74d1cca45c7f7928cf117b6d1180185e
Federico Zivolo <fzivolo@quid.com>
2018-11-30 14:39:40 +0100

    chore: ditch npmrc overwrite and use yarn config set

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,16 @@
+EMAIL="automated+write@quid.com"
+NAME="automated.write"
+AUTH=$AUTOMATED_WRITE_AUTH
+
 # Setup Git user for automatic deploy
-git config --global user.email "automated+write@quid.com"
-git config --global user.name "automated.write"
+git config --global user.email $EMAIL
+git config --global user.name $NAME
 
 # Override the read-only npmrc with one with write permissions
 # the auth key is stored in Travis CI
-bash scripts/npmrc.sh $AUTOMATED_WRITE_AUTH > .npmrc
-
-# Without this, Lerna will not publish because it checks of uncommitted files
-git update-index --assume-unchanged .npmrc
+yarn config set username $NAME
+yarn config set email $EMAIL
+yarn config set _auth $AUTH
 
 # Bump versions, commit, push to git, publish to npm/jfrog
 $(yarn bin)/lerna publish --no-verify-access --registry https://quid.jfrog.io/quid/api/npm/npm-quid/
