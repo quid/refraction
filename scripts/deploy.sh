5bfad959eb1b1e2445e91900b86f91125b723a58
Federico Zivolo <fzivolo@quid.com>
2018-11-30 13:42:03 +0100

    chore: deploy.sh improvements

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ git config --global user.name "automated.write"
 # the auth key is stored in Travis CI
 bash scripts/npmrc.sh $AUTOMATED_WRITE_AUTH > .npmrc
 
-cat .npmrc
-npm whoami
+# Without this, Lerna will not publish because it checks of uncommitted files
+git update-index --assume-unchanged .npmrc
 
 # Bump versions, commit, push to git, publish to npm/jfrog
-$(yarn bin)/lerna publish
+$(yarn bin)/lerna publish --no-verify-access --registry https://quid.jfrog.io/quid/api/npm/npm-quid/
