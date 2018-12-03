4ab33cfb617a6f0aeafc9ee4c2a88465ba5c6c9f
Federico Zivolo <fzivolo@quid.com>
2018-12-03 20:27:42 +0100

    chore: styleguide improvements

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,25 @@
+// @noflow
+const fs = require('fs');
+const override = require('./config-overrides');
+
+const packages = fs
+  .readdirSync('./packages')
+  .filter(pkg => !pkg.startsWith('.'))
+  .map(pkg => ({
+    name: pkg,
+    content: `packages/${pkg}/README.md`,
+    components: `packages/${pkg}/**/*.js`,
+  }));
+
 module.exports = {
-  styleguideDir: './docs',
-  components: 'packages/**/*.js',
+  webpackConfig: override(require('react-scripts/config/webpack.config.dev')),
+  sections: [
+    {
+      name: 'Introduction',
+      content: 'docs/introduction.md',
+    },
+    ...packages,
+  ],
+  getExampleFilename: componentPath =>
+    componentPath.replace(/\.js$/, '.examples.md'),
 };
