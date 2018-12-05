26eade07579adfdc90613d530153dd7bcab47c25
Federico Zivolo <fzivolo@quid.com>
2018-12-05 15:00:21 +0100

    chore: replace example package with init script
    Rather than having an example package, we now have a command that will setup new packages automatically.

---
+++ packages/react-package-template/init.js
@@ -0,0 +1,22 @@
+// @noflow
+module.exports = {
+  destination: {
+    default: function(data) {
+      return `packages/${data.packageName}`;
+    },
+  },
+  prompts: {
+    packageName: {
+      message: 'package name (e.g. button)',
+      validate: function(val) {
+        return (
+          /^([a-z0-9]+\-?)+$/.test(val.trim()) ||
+          'Must be lower + dash-cased string'
+        );
+      },
+    },
+    packageDescription: {
+      message: 'package description',
+    },
+  },
+};
