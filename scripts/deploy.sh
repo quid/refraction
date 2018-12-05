d4a04506f9e0466b26783c0d5e96187d59778e8c
Federico Zivolo <fzivolo@quid.com>
2018-12-05 19:16:57 +0100

    chore: gen automated.write npmrc on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,17 @@
-NPM_EMAIL="automated+write@quid.com"
-NPM_NAME="automated.write"
-NPM_AUTH=$AUTOMATED_WRITE_AUTH
-
 # Setup Git user for automatic deploy
 git config --global user.email $GITHUB_EMAIL
 git config --global user.name $GITHUB_NAME
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/quid/ui-framework.git
 
-# Override the read-only npmrc with one with write permissions
-# the auth key is stored in Travis CI
-yarn config set username $NPM_NAME
-yarn config set email $NPM_EMAIL
-yarn config set _auth $NPM_AUTH
-
 # Lerna can publish only if on master, Travis CI goes in detached state initially
 git checkout master
+
+# Override the read-only npmrc with one with write permissions
+# the auth key is stored in Travis CI
+bash scripts/npmrc.sh $AUTOMATED_WRITE_AUTH > .npmrc
+
+# Without this, Lerna will not publish because it checks for uncommitted files
+git update-index --assume-unchanged .npmrc
 
 # Bump versions, commit, push to git, publish to npm/jfrog
 $(yarn bin)/lerna publish --yes --no-verify-access --registry https://quid.jfrog.io/quid/api/npm/npm-quid/
