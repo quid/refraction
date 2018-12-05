f7120620961ce11ae9015a0008a1659b8a76dbc8
Federico Zivolo <fzivolo@quid.com>
2018-12-05 14:22:58 +0100

    chore: styleguide improvements

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,19 +2,27 @@
 const fs = require('fs');
 const override = require('./config-overrides').webpack;
 
+const ignore = [
+  '**/__mocks__/**',
+  '**/*.test.js',
+  '**/*.spec.js',
+  '**/node_modules/**',
+];
+
 const packages = fs
   .readdirSync('./packages')
   .filter(pkg => !pkg.startsWith('.'))
   .filter(pkg => pkg !== '_example')
   .map(pkg => ({
-    name: pkg,
     content: `packages/${pkg}/README.md`,
     components: `packages/${pkg}/**/*.js`,
+    ignore,
   }));
 
 module.exports = {
   webpackConfig: override(require('react-scripts/config/webpack.config.dev')),
   ignore: ['**/*.{test,spec}.js', '**/dist/**/*'],
+  skipComponentsWithoutExample: true,
   sections: [
     {
       name: 'Introduction',
@@ -25,6 +33,5 @@ module.exports = {
       sections: packages,
     },
   ],
-  getExampleFilename: componentPath =>
-    componentPath.replace(/\.js$/, '.examples.md'),
+  require: ['@quid/theme/fonts/asap/index.css', './src/index.css'],
 };
