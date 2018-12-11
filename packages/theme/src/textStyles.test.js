bfc1a609b052d42b0f53fd7ab3ce958017cecd4f
Federico Zivolo <fzivolo@quid.com>
2018-12-11 16:05:43 +0100

    test: updated snasphots

--- packages/theme/src/textStyles.test.js
+++ packages/theme/src/textStyles.test.js
@@ -5,15 +5,20 @@ import css from '@emotion/css/macro';
 import { mount } from 'enzyme';
 import textStyles from './textStyles';
 
+// FIXME(fzivolo): right now I can't find a way to disable source maps in these
+// snapshots, once we find a way, get rid of the `.split` call
+
 it('returns a valid CSS', () => {
-  expect(textStyles('normal')({ theme: {} }).styles).toMatchSnapshot();
+  expect(
+    textStyles('normal')({ theme: {} }).styles.split('/*# sourceMappingURL')[0]
+  ).toMatchSnapshot();
 });
 
 it('works with "withFallback" powered styles (css)', () => {
   expect(
     css`
       ${textStyles('link')({ theme: {} })}
-    `.styles
+    `.styles.split('/*# sourceMappingURL')[0]
   ).toMatchSnapshot();
 });
 
