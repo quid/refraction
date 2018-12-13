1a501e1c5f9b62cc6792a4365bf27ab1bc814c0d
Federico Zivolo <fzivolo@quid.com>
2018-12-13 16:46:24 +0100

    feat: add Text component

---
+++ packages/react-core/src/Text/index.test.js
@@ -0,0 +1,29 @@
+// @flow
+import React from 'react';
+import { mount } from 'enzyme';
+import Text from './';
+
+it('renders the expected markup', () => {
+  const wrapper = mount(<Text />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('renders a component with a different tag', () => {
+  const wrapper = mount(<Text as="h1" type="xlarge" />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('renders the plain skeleton', () => {
+  const wrapper = mount(<Text.Skeleton>foobar foobaz</Text.Skeleton>);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('renders a Skeleton with a different tag', () => {
+  const wrapper = mount(<Text.Skeleton as="h1">foobar foo</Text.Skeleton>);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('renders a Skeleton with set width', () => {
+  const wrapper = mount(<Text.Skeleton width="20px" />);
+  expect(wrapper).toMatchSnapshot();
+});
