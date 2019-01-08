97f52353f995df7a0c344e10941641fe56c32029
Federico Zivolo <fzivolo@quid.com>
2019-01-08 12:53:22 +0100

    feat: add new stylis-plugin-focus-visible

---
+++ packages/stylis-plugin-focus-visible/src/index.js
@@ -0,0 +1,35 @@
+// @flow
+const notFocusVisibleRgxp = /:not\(:focus-visible\)/g;
+const focusVisibleRgxp = /:focus-visible/g;
+
+const trim = selector => selector.trim().replace(/\s+/g, ' ');
+
+export default (
+  context: number,
+  content: string,
+  selectors: Array<string>,
+  parents: Array<string>,
+  line: number,
+  column: number,
+  length: number
+) => {
+  if (context === 2) {
+    selectors.forEach((selector, index) => {
+      if (selector.match(notFocusVisibleRgxp)) {
+        const cleanSelector = selector.replace(notFocusVisibleRgxp, ':focus');
+        selectors[index] = trim(`
+          [data-whatinput]:not([data-whatinput="keyboard"]) ${cleanSelector},
+          [data-whatinput]:not([data-whatinput="initial"]) ${cleanSelector}
+        `);
+      } else if (selector.match(focusVisibleRgxp)) {
+        const cleanSelector = selector.replace(focusVisibleRgxp, ':focus');
+        selectors[index] = trim(`
+          [data-whatinput="keyboard"] ${cleanSelector},
+          [data-whatinput="initial"] ${cleanSelector}
+        `);
+      }
+    });
+  }
+
+  return content;
+};
