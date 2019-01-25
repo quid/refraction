4e6e764de5651c28869a083c3f24feaec21a3b8d
Federico Zivolo <fzivolo@quid.com>
2019-01-25 17:49:27 +0100

    chore: enfore license header to all source files

--- src/licenseHeader.js
+++ src/licenseHeader.js
@@ -4,9 +4,3 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-// @flow
-import colors from './colors';
-
-it('exposes the right colors', () => {
-  expect(colors).toMatchSnapshot();
-});
