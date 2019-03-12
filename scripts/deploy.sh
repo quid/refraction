4020d295328421abde2662a10a0859b6d5e249dc
Federico Zivolo <federico.zivolo@gmail.com>
2019-03-12 10:28:34 +0100

    chore: get rid of npmrc generator
    I just realized npmrc accepts environment variables in it, so we don’t need to generate the config file at deploy time, we can simply make it read the env variable we set.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,5 @@ git remote set-url origin https://${GITHUB_TOKEN}@github.com/quid/refraction.git
 # Lerna can publish only if on master, Travis CI goes in detached state initially
 git checkout master
 
-# Override the read-only npmrc with one with write permissions
-# the auth key is stored in Travis CI
-bash scripts/npmrc.sh $AUTOMATED_WRITE_AUTH > .npmrc
-
 # Bump versions, commit, push to git, publish to npm
 $(yarn bin)/lerna publish --yes
