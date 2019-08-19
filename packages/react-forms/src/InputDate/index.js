57ca89e7ebd2947f8f9b4c57d53668a9219e38c7
Gabriel Micko <gabriel.micko@quid.com>
2019-08-19 18:29:17 +0200

    refactor: changes class component to functional with hooks

--- packages/react-forms/src/InputDate/index.js
+++ packages/react-forms/src/InputDate/index.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 // @flow
-import * as React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import parse from 'date-fns/parse';
 import { Manager, Popper, Reference } from 'react-popper';
 import InputText from '../InputText';
@@ -36,126 +36,134 @@ type State = {
   isOpen: boolean,
 };
 
-export default class InputDate extends React.Component<Props, State> {
-  component = React.createRef /*:: <HTMLElement> */();
-  refA = React.createRef /*:: <HTMLElement> */();
-  refB = React.createRef /*:: <HTMLElement> */();
+const InputDate = ({
+  value,
+  onChange, // eslint-disable-line no-unused-vars
+  isOpen: isOpenProp, // eslint-disable-line no-unused-vars
+  onToggle, // eslint-disable-line no-unused-vars
+  min,
+  max,
+  ...props
+}: Props) => {
+  const component = React.createRef();
+  const refA = React.createRef();
+  const refB = React.createRef();
 
-  state = {
-    current: undefined,
-    isOpen: false,
-  };
+  const [current, setCurrent] = useState();
+  const [isOpen, setOpen] = useState(isOpenProp);
 
-  get isOpen(): boolean {
-    return Boolean(this.props.onToggle ? this.props.isOpen : this.state.isOpen);
-  }
-
-  setIsOpen = (isOpen: boolean) =>
-    this.props.onToggle
-      ? this.props.onToggle(isOpen)
-      : this.setState({ isOpen });
-
-  handleOpen = () => this.isOpen === false && this.setIsOpen(true);
-
-  handleClose = /* istanbul ignore next */ () =>
-    void (this.isOpen === true && this.setIsOpen(false));
-
-  handleCloseOnBlur = (evt: MouseEvent) => {
-    const { relatedTarget } = evt;
-    // istanbul ignore next (this is too DOM related)
-    if (
-      this.component.current && relatedTarget instanceof HTMLElement
-        ? !this.component.current.contains(relatedTarget)
-        : true
-    ) {
-      this.setIsOpen(false);
+  useEffect(() => {
+    if (isOpen !== isOpenProp) {
+      setOpen(isOpenProp);
     }
-  };
+  }, [isOpen, isOpenProp]);
 
-  handleInputChange = (evt: SyntheticInputEvent<HTMLInputElement>) => {
-    const current = new Date(evt.target.value);
-    // istanbul ignore else
-    if (!isNaN(current.getTime())) {
-      this.setState({ current });
+  const setIsOpen = useCallback(
+    (isOpen: boolean) => {
+      onToggle ? onToggle(isOpen) : setOpen(isOpen);
+    },
+    [onToggle, setOpen]
+  );
+
+  const handleOpen = useCallback(() => {
+    if (isOpen === false) {
+      setIsOpen(true);
     }
-    this.props.onChange(evt.target.value);
-  };
+  }, [isOpen, setIsOpen]);
 
-  handleSelect = (selected: Date) => {
-    this.setIsOpen(false);
-    this.props.onChange(toYYYYMMDD(selected));
-  };
+  const handleClose = useCallback(() => {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+  }, [isOpen, setIsOpen]);
 
-  preventDefault = (evt: Event) => evt.preventDefault();
+  const handleCloseOnBlur = useCallback(
+    (evt: MouseEvent) => {
+      const { relatedTarget } = evt;
+      // istanbul ignore next (this is too DOM related)
+      if (
+        component.current && relatedTarget instanceof HTMLElement
+          ? !component.current.contains(relatedTarget)
+          : true
+      ) {
+        setIsOpen(false);
+      }
+    },
+    [component, setIsOpen]
+  );
 
-  render() {
-    const {
-      value,
-      onChange, // eslint-disable-line no-unused-vars
-      isOpen: isOpenProp, // eslint-disable-line no-unused-vars
-      onToggle, // eslint-disable-line no-unused-vars
-      min,
-      max,
-      ...props
-    } = this.props;
+  const handleInputChange = useCallback(
+    (evt: SyntheticInputEvent<HTMLInputElement>) => {
+      const current = new Date(evt.target.value);
+      // istanbul ignore else
+      if (!isNaN(current.getTime())) {
+        setCurrent(current);
+      }
+      onChange(evt.target.value);
+    },
+    [setCurrent, onChange]
+  );
 
-    const { current } = this.state;
+  const handleSelect = useCallback(
+    (selected: Date) => {
+      setIsOpen(false);
+      onChange(toYYYYMMDD(selected));
+    },
+    [setIsOpen, onChange]
+  );
 
-    const isOpen = this.isOpen;
+  const preventDefault = useCallback((evt: Event) => evt.preventDefault(), []);
+  const dateValue = parse(value);
+  const isDateValid = !isNaN(dateValue.getTime());
 
-    const dateValue = parse(value);
-    const isDateValid = !isNaN(dateValue.getTime());
-
-    return (
-      <MouseOutside
-        onClickOutside={this.handleClose}
-        refs={[this.refA, this.refB]}
-      >
-        {() => (
-          <Manager>
-            <Reference>
-              {({ ref }) => (
-                <InputText
-                  type="date"
-                  renderAddon={({ marginRightClass }) => (
-                    <Icon
-                      name="calendar"
-                      className={marginRightClass}
-                      onClick={!props.disabled ? this.handleOpen : undefined}
-                    />
-                  )}
-                  onFocus={this.handleOpen}
-                  focus={isOpen}
-                  onBlur={this.handleCloseOnBlur}
-                  value={value}
-                  onChange={this.handleInputChange}
-                  onClick={this.preventDefault}
-                  min={min}
-                  max={max}
-                  ref={mergeRefs(ref, this.refA)}
-                  {...props}
-                />
-              )}
-            </Reference>
-            {isOpen && (
-              <Popper placement="bottom">
-                {({ ref, style }) => (
-                  <Calendar
-                    onChangeCurrent={current => this.setState({ current })}
-                    current={current || (isDateValid ? dateValue : new Date())}
-                    onSelect={this.handleSelect}
-                    selected={isDateValid ? dateValue : undefined}
-                    minDate={min ? new Date(min) : undefined}
-                    maxDate={max ? new Date(max) : undefined}
-                    ref={mergeRefs(ref, this.component, this.refB)}
-                    style={style}
+  return (
+    <MouseOutside onClickOutside={handleClose} refs={[refA, refB]}>
+      {() => (
+        <Manager>
+          <Reference>
+            {({ ref }) => (
+              <InputText
+                type="date"
+                renderAddon={({ marginRightClass }) => (
+                  <Icon
+                    name="calendar"
+                    className={marginRightClass}
+                    onClick={!props.disabled ? handleOpen : undefined}
                   />
                 )}
-              </Popper>
+                onFocus={handleOpen}
+                focus={isOpen}
+                onBlur={handleCloseOnBlur}
+                value={value}
+                onChange={handleInputChange}
+                onClick={preventDefault}
+                min={min}
+                max={max}
+                ref={mergeRefs(ref, refA)}
+                {...props}
+              />
             )}
-          </Manager>
-        )}
-      </MouseOutside>
-    );
-  }
-}
+          </Reference>
+          {isOpen && (
+            <Popper placement="bottom">
+              {({ ref, style }) => (
+                <Calendar
+                  onChangeCurrent={current => setCurrent(current)}
+                  current={current || (isDateValid ? dateValue : new Date())}
+                  onSelect={handleSelect}
+                  selected={isDateValid ? dateValue : undefined}
+                  minDate={min ? new Date(min) : undefined}
+                  maxDate={max ? new Date(max) : undefined}
+                  ref={mergeRefs(ref, component, refB)}
+                  style={style}
+                />
+              )}
+            </Popper>
+          )}
+        </Manager>
+      )}
+    </MouseOutside>
+  );
+};
+
+export default InputDate;
