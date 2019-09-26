117ea715c4d4aaba18b7a786ddf8b5282293108e
Federico Zivolo <federico.zivolo@gmail.com>
2019-09-26 17:42:57 +0200

    fix: trigger change event on InputNumber step up/down

--- packages/react-forms/src/InputNumber/index.test.js
+++ packages/react-forms/src/InputNumber/index.test.js
@@ -15,27 +15,37 @@ it('renders the expected markup', () => {
 });
 
 it('increments on click on up arrow', () => {
-  const stepUp = jest.fn();
-  const wrapper = mount(<InputNumber defaultValue={0} />);
-  wrapper.find('input').getDOMNode().stepUp = stepUp;
+  const handleChange = jest.fn();
+  const wrapper = mount(
+    <InputNumber defaultValue={0} onChange={handleChange} />
+  );
+  const input = wrapper.find('input').getDOMNode();
+  input.stepUp = jest.fn(() => (input.value = String(Number(input.value) + 1)));
   wrapper
     .find('button')
     .at(0)
     .simulate('click');
 
-  expect(stepUp).toBeCalled();
-  expect(stepUp.mock.calls.length).toBe(1);
+  expect(input.stepUp).toBeCalledTimes(1);
+  expect(handleChange).toBeCalledTimes(1);
+  expect(handleChange.mock.calls[0][0].target.value).toBe('1');
 });
 
 it('decrements on click on down arrow', () => {
-  const stepDown = jest.fn();
-  const wrapper = mount(<InputNumber defaultValue={0} />);
-  wrapper.find('input').getDOMNode().stepDown = stepDown;
+  const handleChange = jest.fn();
+  const wrapper = mount(
+    <InputNumber defaultValue={0} onChange={handleChange} />
+  );
+  const input = wrapper.find('input').getDOMNode();
+  input.stepDown = jest.fn(
+    () => (input.value = String(Number(input.value) - 1))
+  );
   wrapper
     .find('button')
     .at(1)
     .simulate('click');
 
-  expect(stepDown).toBeCalled();
-  expect(stepDown.mock.calls.length).toBe(1);
+  expect(input.stepDown).toBeCalledTimes(1);
+  expect(handleChange).toBeCalledTimes(1);
+  expect(handleChange.mock.calls[0][0].target.value).toBe('-1');
 });
