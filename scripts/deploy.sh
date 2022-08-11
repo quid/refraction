250a8d8676e99b82fdfb468db09b59f0727befd0
Yu-Hsuan Chuang (Flora) <flora831007@gmail.com>
2022-08-11 11:20:12 +0800

    test: branch canary publish settings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,11 @@ git config --global user.name $GITHUB_NAME
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/quid/refraction.git
 
 # Lerna can publish only if on master, Travis CI goes in detached state initially
-git checkout master
+git checkout feat/dropdown-support-customize-dropdownlist
 
 # Override the read-only npmrc with one with write permissions
 # the auth key is stored in Travis CI
 bash scripts/npmrc.sh $AUTOMATED_WRITE_AUTH > .npmrc
 
 # Bump versions, commit, push to git, publish to npm
-$(yarn bin)/lerna publish --yes
+$(yarn bin)/lerna publish --yes --canary
